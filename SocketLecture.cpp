b72b8414fa9332aec2fdff82192d9034ad516e5d
Yodle_94 <sj_artist@naver.com>
2023-12-16 17:25:14 +0900

    Create Thread (WINAPI)

--- SocketLecture.cpp
+++ SocketLecture.cpp
@@ -1,6 +1,39 @@
 ﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+DWORD WINAPI ThreadFunc(LPVOID pParam)
+{
+	cout << "[ThreadFunc] Start" << endl;
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "[ThreadFunc] " << i << endl;
+	}
+
+	cout << "[ThreadFunc] End" << endl;
+	return 0;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	DWORD dwThreadID = 0;
+	HANDLE hThread = ::CreateThread(
+		NULL,
+		0,
+		ThreadFunc,
+		NULL,
+		0,
+		&dwThreadID
+	);
+
+	for (int i = 0; i < 10; ++i)
+		cout << "[main] " << i << endl;
+
+	::CloseHandle(hThread);
+
+	return 0;
 }
+
+
